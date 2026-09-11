96c39abe9606aeea7b8b583dfa6e2075355acf21
Tommy Miland <kontakt@tmiland.com>
2026-09-11 11:16:39 +0200

    electron-modern: use GetCreationContext().ToLocalChecked() (universal across V8 9.x-12.x; Checked() convenience missing on old V8)

--- src/bindings/text-buffer-wrapper.cc
+++ src/bindings/text-buffer-wrapper.cc
@@ -935,7 +935,7 @@ void TextBufferWrapper::load(const Nan::FunctionCallbackInfo<Value> &info) {
   if (!force && text_buffer.is_modified()) {
     Local<Value> argv[] = {Nan::Null(), Nan::Null()};
     auto callback = info[0].As<Function>();
-    Nan::Call(callback, callback->GetCreationContextChecked()->Global(), 2, argv);
+    Nan::Call(callback, callback->GetCreationContext().ToLocalChecked()->Global(), 2, argv);
     return;
   }
 
@@ -1039,7 +1039,7 @@ void TextBufferWrapper::base_text_matches_file(const Nan::FunctionCallbackInfo<V
     bool result = std::equal(file_contents.begin(), file_contents.end(), text_buffer.base_text().begin());
     Local<Value> argv[] = {Nan::Null(), Nan::New<Boolean>(result)};
     auto callback = info[0].As<Function>();
-    Nan::Call(callback, callback->GetCreationContextChecked()->Global(), 2, argv);
+    Nan::Call(callback, callback->GetCreationContext().ToLocalChecked()->Global(), 2, argv);
   }
 }
 
